15d19ff57cd6230b8a5e838767c422f1b172587f
Zuyu Cai <43597736+TtChopper@users.noreply.github.com>
2019-11-06 10:03:43 -0800

    Add files via upload

---
+++ Code/information.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+<!--    <link rel="shortcut icon" href="favicon.ico">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Bootstrap css -->
+    <link type="text/css" rel='stylesheet' href="css/bootstrap.min.css">
+
+    <!-- End Bootstrap css -->
+
+
+    <!-- Fancybox -->
+    <link type="text/css" rel='stylesheet' href="css/jquery.fancybox.css">
+    <!-- End Fancybox -->
+
+    <link type="text/css" rel='stylesheet' href="fonts/fonts.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <link type="text/css" data-themecolor="default" rel='stylesheet' href="css/main-default.css">
+
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="mask-l" style="background-color: #fff; width: 100%; height: 100%; position: fixed; top: 0; left:0; z-index: 9999999;"></div> <!--removed by integration-->
+<header>
+    <div class="container b-header__box b-relative">
+
+        <div class="b-header-r b-right b-header-r--icon">
+
+            <div class="b-top-nav-show-slide f-top-nav-show-slide b-right j-top-nav-show-slide"><i class="fa fa-align-justify"></i></div>
+            <nav class="b-top-nav f-top-nav b-right j-top-nav">
+                <ul class="b-top-nav__1level_wrap">
+                    <li class="b-top-nav__1level f-top-nav__1level is-active-top-nav__1level f-primary-b"><a href="#"><i class="fa fa-home b-menu-1level-ico"></i>Home <span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span></a>
+
+                    </li>
+
+                    <li class="b-top-nav__1level f-top-nav__1level f-primary-b">
+                        <a href="#"><i class="fa fa-picture-o b-menu-1level-ico"></i>Portfolio <span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span></a>
+
+                    </li>
+                    <li class="b-top-nav__1level f-top-nav__1level f-primary-b">
+                        <a href="#"><i class="fa fa-code b-menu-1level-ico"></i>Blog <span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span></a>
+
+                    </li>
+                    <li class="b-top-nav__1level f-top-nav__1level f-primary-b b-top-nav-big">
+                        <a href="#"><i class="fa fa-cloud-download b-menu-1level-ico"></i>Pages<span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span></a>
+
+                    </li>
+
+                    <li class="b-top-nav__1level f-top-nav__1level f-primary-b">
+                        <a href="#"><i class="fa fa-folder-open b-menu-1level-ico"></i>Contact us<span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span></a>
+                    </li>
+
+                </ul>
+
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="j-menu-container"></div>
+
+
+<script src="js/jquery/jquery-1.11.1.min.js"></script>
+<!-- bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- end bootstrap -->
+
+
+<!-- Modules -->
+<script src="js/modules/color-themes.js"></script>
+<!-- End Modules -->
+<!-- Google services -->
+<!---<script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['corechart']}]}"></script>--->
+
+<!-- end Google services -->
+<!-- Fancybox -->
+<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+<script src="js/fancybox/jquery.mousewheel.pack.js"></script>
+<script src="js/fancybox/jquery.fancybox.custom.js"></script>
+<!-- End Fancybox -->
+
+<script src="js/cookie.js"></script>
+
+</body>
+</html>
